cec7b35747d673e6f9fe2e158285f430ae810ddf
Davis Ching <davisching1211@gmail.com>
2022-04-07 23:20:00 +0800

    udpate

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 gssg --productionDomain https://davisxching.github.io  
 
-cp -R /Users/davisching/Documents/ghost/static/* /Users/davisching/Documents/github/davisxching.github.io
-cp -R /Users/davisching/Documents/ghost/content/images/* /Users/davisching/Documents/github/davisxching-static.github.io/content/images
+cp -rf ./static/* /Users/davisching/Documents/github/davisxching.github.io
+rm -r ./static
+cp -rf /Users/davisching/Documents/ghost/content/images/* /Users/davisching/Documents/github/davisxching-static.github.io/content/images
 
 find . -type f -name '*.html' | xargs sed -i -e 's#http://localhost:2368#https://davisxching.github.io#g'
 find . -type f -name '*.html' | xargs sed -i -e 's#"/content/images#"https://davisxching-static.github.io/content/images#g'
